a96835a34638d0368190dabc0de843e4e3869798
Xeolia <alexia.serrierb@gmail.com>
2022-01-09 11:59:01 +0100

    Test data base on PSQL container

--- esieaBack-main/src/main/test/esiea/api/VoitureDAOITest.java
+++ esieaBack-main/src/main/test/esiea/api/VoitureDAOITest.java
@@ -1,10 +1,10 @@
-/*
 package esiea.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import esiea.api.VoitureAPI;
 import esiea.dao.VoitureDAO;
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -14,9 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -30,56 +33,46 @@ import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.sun.org.apache.xml.internal.serialize.LineSeparator.Web;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 @WebAppConfiguration
-@ContextConfiguration()
+@ContextConfiguration(initializers = VoitureDAOITest.Initializer.class)
+@AutoConfigureMockMvc
 
-class VoitureAPITest {
+class VoitureDAOITest {
     @Mock
     private MockMvc mock;
     @Mock private Connection mockConnection = mock(Connection.class);
     @Mock private PreparedStatement mockStatement = mock(PreparedStatement.class);
     @Mock private ResultSet resultSet = mock(ResultSet.class);
-    @InjectMocks
-    private static VoitureDAO voitureDAO = new VoitureDAO("test_url", "test_username");
 
+    private static JdbcDatabase postgreSQLContainer;
 
     @Before
     public void setUp() throws Exception {
+        postgreSQLContainer.start();
         mockStatic(DriverManager.class);
         when(DriverManager.getConnection(anyString(), anyString(), null)).thenReturn(mockConnection);
     }
+    @ClassRule
+    public static PostgreSQLContainer PGSQL_CONTAINER= (postgreSQLContainer) new PostgreSQLContainer("postgres:12-alpine")
+            .withDatabaseName("stockcar_test")
+            .withUsername("root")
+            .withPassword("root");
 
-    @Test
-    void testQueryDatabase() throws Exception {
-        when(mockConnection.prepareStatement(anyString())).thenReturn(mockStatement);
-        when(mockStatement.executeQuery(anyString())).thenReturn(resultSet);
-        when(resultSet.getInt(Mockito.anyInt())).thenReturn(1);
-        when(resultSet.getString(anyInt())).thenReturn("john smith");
 
-        final Map<Integer, String> useridToUser = new HashMap<>();
-        voitureDAO.modifierVoiture(1,voi);
-        System.out.println(useridToUser);
+    static class Initializer
+        implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            // setup (postgreSQLContainer.getJdbcUrl()) as "hibernate.connection.url" here...
+        }
     }
-  */
-/*  @Test
-    public void ajouterVoiture() throws Exception {
-        VoitureAPI voitureApi = new VoitureAPI();
-        ObjectMapper mapper = new ObjectMapper();
-        String result = mapper.writeValueAsString(voitureApi);
-        mock
-                .perform(MockMvcRequestBuilders.post("/voiture/add")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(result)
-                )
-                .andExpect(status().isOk());
 
-    }*//*
 
 
 }
-*/
