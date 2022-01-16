fa529bfcc72de213247a84164973f1b2b91f9d1c
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-01-16 15:21:58 +0100

    test5

--- esieaBack-main/src/test/java/esiea/dao/VoitureDAOTest.java
+++ esieaBack-main/src/test/java/esiea/dao/VoitureDAOTest.java
@@ -62,7 +62,15 @@ class VoitureDAOTest {
     void modifierVoiture() throws SQLException {
 
         VoitureDAO vD = new VoitureDAO();
-        vD.ajouterVoiture(honda);
+        Voiture honda2 = new Voiture();
+        honda2.setMarque("HondaTest");
+        honda2.setModele("Civic");
+        honda2.setFinition("R");
+        honda2.setCarburant(Voiture.Carburant.get("E"));
+        honda2.setAnnee(2020);
+        honda2.setPrix(40000);
+        honda2.setKm(40000);
+        vD.ajouterVoiture(honda2);
 
         Voiture databasaVoiture = new VoitureDAO().getVoiture("HondaTest")[0];
         Voiture newVoiture = new Voiture();
